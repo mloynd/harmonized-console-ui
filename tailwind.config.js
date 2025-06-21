b9e73013075b3ef0c105c4f53c7b58e9311b5539
mloynd <mike.loynd@councilfire.ai>
2025-06-21 12:34:29 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#f9fafb",
+        foreground: "#111827",
+      },
+    },
   },
   plugins: [],
 }
